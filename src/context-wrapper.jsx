a22df0a2135fd60433ea8cbdded1c44fc79474fd
DallonF <dallon.feldner@gmail.com>
2015-09-01 12:32:17 -0500

    Aha! Got it!

--- src/context-wrapper.jsx
+++ src/context-wrapper.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+let store = createStore((data, action={}) => data);
 
 export default class ContextWrapper extends React.Component {
   static propTypes = {
@@ -10,7 +14,11 @@ export default class ContextWrapper extends React.Component {
     let Handler = this.props.handler;
     return (
       <div id="context-wrapper">
-        <Handler data={this.props.data} />
+        <Provider store={store}>
+          {() =>
+            <Handler data={this.props.data} locales={['en-US']} />
+          }
+        </Provider>
       </div>
     );
   }
--- src/pages/landing/page.jsx
+++ src/pages/landing/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import reactMixin from 'react-mixin';
+import {IntlMixin} from 'react-intl';
+import {connect} from 'react-redux';
 import { getData } from "../../common/request";
 
-
+@connect(state => ({state}))
+@reactMixin.decorate(IntlMixin)
 export default class LandingPage extends React.Component {
   componentWillMount() {
     console.log("[LandingPage] will mount with server response: ", this.props.data.landing);
@@ -13,6 +17,7 @@ export default class LandingPage extends React.Component {
     return (
       <div id="landing-page">
         <h1>{title}</h1>
+        <pre>{JSON.stringify(this.props.state)}</pre>
       </div>
     );
   }
